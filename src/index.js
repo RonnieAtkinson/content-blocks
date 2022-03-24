42acf338b4c436c863b715494ebbaf2033138bce
Ronnie Atkinson <1983roncode@gmail.com>
2022-03-24 20:10:10 +0000

    Removed the init, it can now be used in the script tag

--- src/index.js
+++ src/index.js
@@ -8,49 +8,7 @@
 // @todo label for attribute
 //
 
-import { FormConnect } from './FormConnect/FormConnect';
-
 //
-// Content Blocks Class
+// Exports
 //
-class ContentBlocks {
-
-    //
-    // Init 
-    // Static method
-    //
-    // [1]
-    // [2]
-    // [3]
-    //
-    static init() {
-        const formEl = document.querySelector('form'); // [1]
-        const customBlocks = { // [2]
-            list: {
-                display: 'List',
-                formControls: {
-                    content: {
-                        label: 'Check me',
-                        element: {
-                            node: 'input',
-                            attributes: {
-                                type: 'checkbox'
-                            }
-                        }
-                    }
-                }
-            }
-        };
-
-        new FormConnect(formEl, customBlocks, { // [3]
-            exclude: {
-                defaultBlocks: ['link']
-            }
-        });
-    };
-}
-
-//
-// Init
-//
-ContentBlocks.init();
+export { default as FormConnect } from './FormConnect/FormConnect';
