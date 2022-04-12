cd8be9be45c087b376251341ec342cbe3479970f
Ronnie Atkinson <1983roncode@gmail.com>
2022-04-12 18:45:22 +0100

    Updated comments

--- src/ContentBlock/FormGroup/FormGroup.js
+++ src/ContentBlock/FormGroup/FormGroup.js
@@ -11,12 +11,8 @@ import DomNameUtils from '../../utils/DomName';
 import OptionUtils from '../../utils/Option';
 
 //
-// Default options
-//
-const defaultOptions = {};
-
-//
-//
+// Variables
+// These variables are used when adding and removing dynamic elements
 //
 const ACTION_ADD = 'add';
 const ACTION_REMOVE = 'remove';
@@ -24,19 +20,26 @@ const ACTION_REMOVE = 'remove';
 //
 // Form group class
 //
+// @usage
+// new FormGroup(parentNodes, variations, index, options);
+//
 export default class FormGroup {
 
     //
-    // Constructor
+    // Class constructor
+    //
+    // @constructs FormGroup
+    //
+    // @param {object || HTMLElement} parentNodes
+    // @param {object} variations
+    // @param {number} index
+    // @param {object} options
     //
     constructor(parentNodes, variations, index, options) {
         this.parentNodes = parentNodes;
         this.variations = variations;
         this.index = index;
-        this.options = {
-            ...defaultOptions,
-            ...options
-        };
+        this.options = options;
     };
 
     //
@@ -82,7 +85,7 @@ export default class FormGroup {
     //
     // Add dynamic buttons
     // These buttons control the dynamic elements
-    // Dynamic elements are elements that can be manually added to or removed from a form-control after it has been rendered to the dom
+    // Dynamic elements are elements that can be manually added to or removed from a form group after it has been rendered to the dom
     //
     // @param {HTMLElement} targetEl
     //
@@ -91,7 +94,7 @@ export default class FormGroup {
     //
     // [1] Create a new add button and append it to targetEl.
     // [2] Create a new remove button and append it to targetEl.
-    //     New content block dynamic elements will always be zero so the remove button can be set to disabled.
+    //     # When a new content block is created the number of dynamic elements will always be zero so the remove button can be disabled.
     //
     addDynamicButtonsTo(targetEl) {
         new DomElement('button') // [1]
@@ -100,84 +103,87 @@ export default class FormGroup {
                 textContent: this.options.text['button:addDynamic'],
                 classList: [...this.getClassNamesFor('button:addDynamic')]
             })
-            .appendTo(targetEl)
+            .appendTo(targetEl);
 
-        new DomElement('button')
+        new DomElement('button') // [2]
             .addAttributes({
                 type: 'button',
                 textContent: this.options.text['button:removeDynamic'],
                 classList: [...this.getClassNamesFor('button:removeDynamic')],
                 disabled: true
             })
-            .appendTo(targetEl) // [2]
+            .appendTo(targetEl);
 
         return this;
     };
 
     //
-    // Add content block form controls
-    // Adds the form-controls for the content type
-    // ie: <div class="form-control"></div>
+    // Add content block form group
+    // Adds a form group
+    // ie: <div class="form-group"></div>
     //
-    // @param {HTMLElement} fieldSetEl
-    // @param {Object} formControls
-    // @param {Object} variations
+    // @param {string} groupName
+    // @param {object} formGroup
     //
     // @throws
-    // Will throw an error if form controls is not an object.
-    // Will throw an error if form controls length is zero.
+    // Will throw an error if theres no parent element.
     //
     // @usage
-    // this.addContentBlockFormControls(fieldsetEl, formControls, variations);
+    // this.add(groupName, formGroup);
     //
-    // [1] If the form controls parameter is not an object or its an empty object, throw an error
-    // [2] For each key in form controls:
-    // [3] Define hasNode
-    //     # Does formcontrol.element have a node key
-    // [4] Define hasVariations
-    //     # Is the form control key 'variation'
+    // [1] Get the tab value from the formGroup object.
+    //     # Should be a string
+    // [2] Set the parentEl using a ternary.
+    //     # If the parentNodes length is greater than zero then theres tabs
+    //     # Otherwise no tabs
+    //
+    // [3] If parentEl is null, undfined, or empty it could no be set so throw an error.
+    //
+    // [4] Define hasNode
+    //     # Does formGroup.element have a 'node' key
+    //     # Convert to a boolean
+    // [5] Define hasVariations
+    //     # Is the group name 'variation'
     //     # Does the variations parameter have a length greater than 0
     //     # Does this iteration have a node
-    // [5] Define isDynamic
+    // [6] Define isDynamic
     //     # Is the dynamic key truthy
-    // [6] Set the element type based on is dynamic
+    //     # Convert to a boolean
+    // [7] Set the element type based on is dynamic
     //     # Dynamic elements are placed in a fieldset with a legend
     //     # Non-dynamic elements are placed in a div with a label
-    // [7] Add a new form control element
+    // [8] Add a new form group element
     //     # This will be the parent node
-    // [8] Define a placeholder for the node type
-    //     # Defining it here will mean any scopes up the chain have access to it.
-    // [9] If this iteration is dynamic
+    // [9] Define a placeholder for the node type
+    //     # Defining it here will mean any scopes down the chain have access to it.
+    // [10] If this iteration is dynamic
     //     # Will render with zero inputs initally, the user will be able to add them as needed
-    //     # Dynamic form controls will be a fieldset
-    //     # Add a legend node to [7] (fieldset)
-    //     # Add the buttons to [7] (fieldset) that allow inputs to be added and removed
-    //     # If a form control is dynamic it wont initally have nodes so just continue with the next iteration
-    //     
-    //     If this iteration is not dynamic
-    //     # Add a label to [7] (div)
+    //     # Dynamic form groups will be a fieldset
+    //     # Add a legend node to [8] (fieldset)
+    //     # Add the buttons to [8] (fieldset) that allow inputs to be added and removed
+    //     # If a form group is dynamic it wont initally have nodes so just continue with the next iteration.
     //
-    // [10] If this iteration has a node
-    //      # Create the node and add it to [7] (div)
-    //      # Assign a reference of it to [8]
+    // [11] If this iteration has a node
+    //      # Create a label element and appaned it to the form group.
+    //      # Create a form control node and append it to the label.
     //
-    // [11] If this iteration has variations
-    //      # For each variation in the variations parameter:
+    // [12] If this iteration has variations
+    //      # For each variation:
     //      # Create a new node for the variation.
-    //      # Add it as a child to the parent variation node [8] & [10].
+    //      # Append it to the form control node [9] & [11].
     //
     add(groupName, formGroup) {
-        const tabName = formGroup.tab;
-        const parentEl = (Object.keys(this.parentNodes).length > 0) ? this.parentNodes[tabName] : this.parentNodes;
+        const tabName = formGroup.tab; // [1]
+        const parentEl = (Object.keys(this.parentNodes).length > 0) ? this.parentNodes[tabName] : this.parentNodes; // [2]
 
-        if (CheckValue.isNullUndefinedEmpty(parentEl)) throw new Error(`${formGroup.label} has been assigned to a tab that hasn't been defined`);
+        if (CheckValue.isNullUndefinedEmpty(parentEl)) throw new Error(`${formGroup.label} has been assigned to a tab that hasn't been defined`); // [3]
 
-        const hasNode = !!((formGroup.element || {}).node); // [3]
-        const hasVariations = groupName === 'variation' && !!(Object.keys(this.variations || {}).length) && hasNode; // [4]
-        const isDynamic = !!((formGroup || {}).dynamic); // [5]
-        const elType = (isDynamic) ? 'fieldset' : 'div'; // [6]
+        const hasNode = !!((formGroup.element || {}).node); // [4]
+        const hasVariations = groupName === 'variation' && !!(Object.keys(this.variations || {}).length) && hasNode; // [5]
+        const isDynamic = !!((formGroup || {}).dynamic); // [6]
+        const elType = (isDynamic) ? 'fieldset' : 'div'; // [7]
 
-        const { el: formGroupEl } = new DomElement(elType) // [7]
+        const { el: formGroupEl } = new DomElement(elType) // [8]
             .addAttributes({
                 classList: [
                     ...this.getClassNamesFor('form:group'),
@@ -188,21 +194,23 @@ export default class FormGroup {
             })
             .appendTo(parentEl);
 
-        let thisNode; // [8]
+        let thisNode; // [9]
 
-        if (isDynamic) { // [9]
+        if (isDynamic) { // [10]
             new DomElement('legend').addAttributes({ textContent: (formGroup || {}).label }).appendTo(formGroupEl);
             this.addDynamicButtonsTo(formGroupEl);
             return;
         };
 
-        if (hasNode) { // [10]
+        if (hasNode) { // [11]
+            // Create a label element
             const { el: labelEl } = new DomElement('label')
                 .addAttributes({
                     textContent: ((formGroup || {}).label),
                 })
                 .appendTo(formGroupEl);
 
+            // Create the node
             ({ el: thisNode } = new DomElement(formGroup.element.node)
                 .addAttributes({
                     ...formGroup.element.attributes || {},
@@ -228,7 +236,6 @@ export default class FormGroup {
         };
     };
 
-
     //
     // Toggle dynamic remove button
     // Conditionally toggles the disabled attribute on the remove button
@@ -236,7 +243,9 @@ export default class FormGroup {
     // Sets the disabled status of the remove button based on the number of inputs
     // Zero inputs = disabled
     // More than zero = enabled
-    // 
+    //
+    // @static
+    //
     // @param {HTMLElement} targetEl
     // @param {number} inputLength
     // @param {string} action
@@ -277,11 +286,13 @@ export default class FormGroup {
     //
     // Add dynamic element
     // Adds a dynamic element to the DOM.
-    // Dynamic elements are elements that can be manually added to or removed from a form-control after it has been rendered to the dom.
+    // Dynamic elements are elements that can be manually added to or removed from a form group after it has been rendered to the dom.
+    //
+    // @static
     //
     // @param {HTMLElement} targetEl
     // @param {string} elType
-    // @param {Live NodeList} allInputs
+    // @param {live NodeList} allInputs
     //
     // @usage
     // this.addDynamicElement(targetEl, elType, allInputs);
@@ -289,16 +300,17 @@ export default class FormGroup {
     // @example
     // this.addDynamicElement();
     //
-    // [1] Get the data-index from the closest fieldset.
-    // [2] Destruct the name and type from the closest form control dataset.
+    // [1] Get the data-index from the closest content block.
+    // [2] Destruct the name, type, and placeholder from the closest form group dataset.
     //     # data-name, data-type.
     // [3] Define a new params object.
-    //     # Only needs the name at this point.
-    // [4] If there was a data-type on the parent [2] add a 'type' attribute to the params object:
+    //     # Only needs the name and classlist at this point.
+    // [4] If there was a data-type [2] on the content block add a 'type' attribute to the params object:
     //     # This is in a conditional statement because not all inputs need a type param.
     //     # textarea for example doesn't need one. 
-    // [5] Create a new dom element using the params object and insert before targetEl
-    // [6] Call toggleDynamicRemoveButton();
+    // [5] If there was a data-placeholder [2] on the content block add a 'placeholder' to the params object.
+    // [6] Create a new dom element using the params object and insert before targetEl
+    // [7] Call toggleDynamicRemoveButton();
     //     # Disables the remove button if required.
     //
     static addDynamicElement(targetEl, elType, allInputs, classes) {
@@ -309,10 +321,10 @@ export default class FormGroup {
             classList: [...classes['form:control']]
         };
         if (inputType) params.type = inputType; // [4]
-        if (inputPlaceholder) params.placeholder = inputPlaceholder;
+        if (inputPlaceholder) params.placeholder = inputPlaceholder; // [5]
 
-        new DomElement(elType).addAttributes(params).insertBeforeEl(targetEl); // [5]
-        FormGroup.toggleDynamicRemoveButton(targetEl.nextElementSibling, allInputs.length, ACTION_ADD); // [6]
+        new DomElement(elType).addAttributes(params).insertBeforeEl(targetEl); // [6]
+        FormGroup.toggleDynamicRemoveButton(targetEl.nextElementSibling, allInputs.length, ACTION_ADD); // [7]
 
         return this;
     };
@@ -320,10 +332,12 @@ export default class FormGroup {
     //
     // Remove dynamic element
     // Removes a dynamic element from the DOM
-    // Dynamic elements are elements that can be manually added to or removed from a form-control after it has been rendered to the dom
+    // Dynamic elements are elements that can be manually added to or removed from a form group after it has been rendered to the dom
+    //
+    // @static
     //
     // @param {HTMLElement} targetEl
-    // @param {Live NodeList} allInputs
+    // @param {live NodeList} allInputs
     //
     // @usage
     // this.removeDynamicElement(targetEl, allInputs);
